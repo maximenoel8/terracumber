4931e812c3545f4d5571a9dcb8141fdf8cefa9c7
Maxime Noel <maxime.noel@suse.com>
2024-08-15 14:48:56 +1200

    Fix quote issue

--- terracumber/terraformer.py
+++ terracumber/terraformer.py
@@ -76,7 +76,7 @@ class Terraformer:
                     node_mu_repos = repos.get(node, None)
                     replacement_list = ["additional_repos = {"]
                     for name, url in node_mu_repos.items():
-                        replacement_list.append(f'\n{" " * 4}{name} = "{url}"')
+                        replacement_list.append(f'\n    "{name}" = "{url}"')
                     replacement_list.append("\n  }")
                     replacement = ''.join(replacement_list)
                     placeholder = '//' + node + '_additional_repos'
